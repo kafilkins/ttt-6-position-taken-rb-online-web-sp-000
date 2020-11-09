82a0e7b0108692ac04c8a753fef86062066df729
Keith Filkins <kafilkins@me.com>
2020-11-09 21:53:58 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def position_taken?(board, index)
-  if board[index] == "X" or board[index] == "O" 
+  if board[index] == "X" or board[index] == "O"
     return true
     else
     return false
