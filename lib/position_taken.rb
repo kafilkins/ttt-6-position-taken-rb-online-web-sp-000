d61361bcf610f270322e291b683a150a3ff20548
Keith Filkins <kafilkins@me.com>
2020-11-09 21:11:07 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,8 @@
 def position_taken?(board, index)
 end
 
-if board[" "] == [" "] && index[0] == 0
-  return "false"
-end
+def position_taken?(board[" "], index[0])
+  if board == " " && index[0] == 0
+    return "false"
+  end
+  
